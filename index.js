15a08160f8caef61f628322e723c6c8e18af05b9
Maksym <maksymel.lykhovyd@gmail.com>
2024-11-15 19:12:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-fetch('/api/pixels')
-    .then(response => response.json())
-    .then(data => {
-        for (const pixelId in data) {
-            const pixel = document.querySelector(`[data-pixel-id="${pixelId}"]`);
-            if (pixel) {
-                pixel.style.backgroundColor = data[pixelId].color;
-                pixel.addEventListener('click', () => {
-                    window.open(data[pixelId].url, '_blank');
-                });
-            }
-        }
-    });
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Симуляция базы данных (в реальности используйте MongoDB, PostgreSQL и т.д.)
+const pixelData = {};
+
+app.use(bodyParser.json());
+
+// Получение данных о пикселях
+app.get('/api/pixels', (req, res) => {
+    res.json(pixelData);
+});
+
+// Сохранение данных о купленном пикселе
+app.post('/api/buyPixel', (req, res) => {
+    const { pixelId, color, url } = req.body;
+    if (!pixelData[pixelId]) { // Проверка, что пиксель не куплен
+        pixelData[pixelId] = { color, url };
+        res.status(200).send('Pixel purchased successfully');
+    } else {
+        res.status(400).send('Pixel already purchased');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
